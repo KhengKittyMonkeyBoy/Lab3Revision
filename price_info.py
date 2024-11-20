12e6e09b823b00641ab8d42340978bda7cf12ca8
Alfie <alfie.23@ichat.sp.edu.sg>
2024-11-21 05:08:50 +0800

    Made a ton of changes

--- price_info.py
+++ price_info.py
@@ -1,27 +1,34 @@
-price_list = { 'apple': 1.20, 'orange': 1.40,'watermelon': 6.50, 'pineapple': 2.70, 'pear': 0.90, 'papaya': 2.95,'pomegranate': 4.95
-}
-
-quantity_list = {'apple': 5,'orange': 5,'watermelon': 1,'pineapple': 2,'pear': 10,'papaya': 1,'pomegranate': 2
-}
+price_list = {'apple': 1.20, 'orange': 1.40, 'watermelon': 6.50, 'pineapple': 2.70, 'pear': 0.90, 'papaya': 2.95, 'pomegranate': 4.95}
+quantity_list = {'apple': 5, 'orange': 5, 'watermelon': 1, 'pineapple': 2, 'pear': 10, 'papaya': 1, 'pomegranate': 2}
 
 def total_cost_shopping():
-    total_cost = 0
-    for fruit in price_list.keys():
-        if fruit in quantity_list:
-            total_cost += price_list[fruit] * quantity_list[fruit]
+    total_cost = 0  # Initialize total_cost outside the loop
+    for item in price_list:
+        if item in quantity_list:
+            item_cost = price_list[item] * quantity_list[item]
+            total_cost += item_cost  # Add the cost of this item to the total cost
     
-    print("total cost =", total_cost)
+    # Print the total cost after the loop finishes
+    print("The total cost of your shopping is:", total_cost)
+
+
+
+
 
 def cost_of_fruits(fruit, quantity):
-    cost = 0
-    if fruit in price_list:
-        cost = quantity * price_list[fruit]
-    
-    print("cost of", quantity, fruit, "=", cost)
+    for key in price_list.keys():
+        if key == fruit:
+            cost = quantity*price_list[key]
+            break
+
+    print("cost of ", quantity, fruit, "=", cost)
+
 
 def main():
+
     cost_of_fruits('apple', 10)
     total_cost_shopping()
+
 
 if __name__ == "__main__":
     main()
---
+++ test_employee_info.py
@@ -0,0 +1,5 @@
+import pytest
+from employee_info import get_employees_by_age_range, calculate_average_salary, get_employees_by_dept
+
+#sample data for testing
+employees=[]
